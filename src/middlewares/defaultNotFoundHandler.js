6fce02a9aed862a09d0872435b08689bd72e8ab9
OnlyBrad <bradonly420@gmail.com>
2023-09-08 09:37:00 -0400

    version 2.0.0 - defaultNotFoundHandler takes into account the Accept header

--- src/middlewares/defaultNotFoundHandler.js
+++ src/middlewares/defaultNotFoundHandler.js
@@ -1,9 +1,35 @@
 module.exports = 
 /**
- * @param {import("../Request")} req 
- * @param {import("../Response")} res 
+ * @param {import("../core/Request")} req 
+ * @param {import("../core/Response")} res 
  */
 function defaultNotFoundHandler(req, res) {
-    return res.status(404).text("404 Page Not Found.");
+    res = res.status(404);
+    const message = "404 Page Not Found.";
+    
+    if(req.wantsHtml()) {
+        return res.html(`<!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>404 - Not Found</title>
+        </head>
+        <body>
+            <p>${message}</p>
+        </body>
+        </html>`);
+    }
+
+    if(req.wantsJson()) {
+        return res.json({message});
+    }
+
+    if(req.wantsXml()) {
+        return res.setHeader("Content-type", "application/xml")
+            .text(`<?xml version='1.0' encoding='UTF-8'?><message>${message}</message>`)
+    }
+
+    return res.text(message);
 }
 
